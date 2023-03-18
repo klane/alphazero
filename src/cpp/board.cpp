ec8cf200dc2c1bd9fd994de547666878bc7cba5d
Kevin Lane <lane.kevin.a@gmail.com>
2023-03-17 22:20:53 -0700

    Add basic board class

---
+++ src/cpp/board.cpp
@@ -0,0 +1,81 @@
+#include "board.hpp"
+
+namespace alphazero {
+
+Board::Board(const uint8_t rows, const uint8_t cols) :
+    Board::Board({Position(rows * cols), Position(rows * cols)}, rows, cols)
+{}
+
+Board::Board(const BoardPosition start_positions, const uint8_t rows, const uint8_t cols) :
+    rows(rows),
+    cols(cols),
+    start_positions(start_positions),
+    positions(start_positions)
+{}
+
+Position Board::empty() {
+    return filled().flip();
+}
+
+Position Board::filled() {
+    return positions[BLACK] | positions[WHITE];
+}
+
+bool Board::is_full() {
+    return filled().all();
+}
+
+void Board::make_move(Color player, Position move) {
+    positions[player] |= move;
+}
+
+int Board::num_empty() {
+    return empty().count();
+}
+
+Board Board::peek(Color player, Position move) {
+    Board board(*this);
+    board.make_move(player, move);
+    return board;
+}
+
+int Board::player_count(Color player) {
+    return positions[player].count();
+}
+
+Position Board::player_position(Color player) {
+    return positions[player];
+}
+
+void Board::reset() {
+    positions = start_positions;
+}
+
+Position Board::valid_moves(Color player) {
+    return empty();
+}
+
+Coordinate position_to_coordinate(Position position, const uint8_t rows, const uint8_t cols) {
+    const Position::size_type index = position.find_first();
+
+    if (index == Position::npos)
+        return Coordinate(-1, -1);
+
+    return Coordinate((int) index / rows, index % cols);
+}
+
+PositionSet split_position(Position position) {
+    PositionSet position_set;
+    Position::size_type index = position.find_first();
+
+    while (index != Position::npos) {
+        Position temp(position.size());
+        temp.set(index);
+        position_set.push_back(temp);
+        index = position.find_next(index);
+    }
+
+    return position_set;
+}
+
+}
---
+++ src/cpp/board.hpp
@@ -0,0 +1,60 @@
+#include <array>
+#include <vector>
+
+#include <boost/dynamic_bitset.hpp>
+
+namespace alphazero {
+
+const uint8_t NUM_PLAYERS = 2;
+
+using Position = boost::dynamic_bitset<>;
+using PositionSet = std::vector<Position>;
+using BoardPosition = std::array<Position, NUM_PLAYERS>;
+
+enum Color: uint8_t {
+    BLACK = 0,
+    WHITE = 1
+};
+
+struct Coordinate {
+    const uint8_t row;
+    const uint8_t col;
+
+    Coordinate(const uint8_t row, const uint8_t col) :
+        row(row),
+        col(col)
+    {}
+};
+
+class Board {
+    public:
+        const uint8_t rows;
+        const uint8_t cols;
+        const BoardPosition start_positions;
+        BoardPosition positions;
+
+        Board(const uint8_t rows, const uint8_t cols);
+        Board(
+            const BoardPosition start_positions, const uint8_t rows, const uint8_t cols
+        );
+
+        Position empty();
+        Position filled();
+        bool is_full();
+        int num_empty();
+        int player_count(Color player);
+        Position player_position(Color player);
+
+        void make_move(Color player, Position move);
+        Board peek(Color player, Position move);
+        void reset();
+        Position valid_moves(Color player);
+};
+
+Coordinate position_to_coordinate(
+    Position position, const uint8_t rows, const uint8_t cols
+);
+
+PositionSet split_position(Position position);
+
+}
