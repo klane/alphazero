2e9fec0898a206727e9f4a5a4c3f257c19dbda00
Kevin Lane <lane.kevin.a@gmail.com>
2023-03-23 22:16:43 -0700

    Draw valid moves

--- src/python/app.py
+++ src/python/app.py
@@ -1,8 +1,9 @@
 from itertools import product
 
 import pygame as pg
+from pygame import gfxdraw
 
-from alphazero.engine import Board
+from alphazero.engine import Board, Color, Position, split_position
 
 CAPTION = 'AlphaZero'
 PLAYER1_COLOR = pg.Color('black')
@@ -20,9 +21,11 @@ class App:
         self.game = Board(8, 8)
         self.size = size
         self.running = True
+        self.current_player = Color.BLACK
 
         cell_height = self.size // self.game.rows
         cell_width = self.size // self.game.cols
+        self.move_radius = min(cell_height // 8, cell_width // 8)
 
         screen_size = size, size
         self.screen = pg.display.set_mode(screen_size)
@@ -34,6 +37,11 @@ class App:
             y = r * cell_height
             cell = pg.Rect(x, y, cell_width, cell_height)
             self.cells.append(cell)
+
+    def draw_circle(self, position: Position, radius: int, color: pg.Color) -> None:
+        x, y = self.cells[position.find_first()].center
+        gfxdraw.aacircle(self.screen, x, y, radius, color)
+        gfxdraw.filled_circle(self.screen, x, y, radius, color)
 
     def event_loop(self) -> None:
         for event in pg.event.get():
@@ -53,6 +61,9 @@ class App:
 
         for c in self.cells:
             pg.draw.rect(self.screen, BACKGROUND_COLOR, c, 2)
+
+        for position in split_position(self.game.valid_moves(self.current_player)):
+            self.draw_circle(position, self.move_radius, MOVE_COLOR)
 
         pg.display.update()
 
